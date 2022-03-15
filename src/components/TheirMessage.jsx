d20273fad9a46bf065cfed5e29fde8244711d2c6
Irtaza12 <irtazazulfiqar6tn@gmail.com>
2022-03-15 06:34:21 -0700

    Irtaza12

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -17,7 +17,7 @@ const TheirMessage = ({ lastMessage, message }) => {
               className="message-image"
               style={{ marginLeft: isFirstMessageByUser ? '4px' : '48px' }}
             />
-          )
+          ) 
           : (
             <div className="message" style={{ float: 'left', backgroundColor: '#CABCDC', marginLeft: isFirstMessageByUser ? '4px' : '48px' }}>
               {message.text}
