c37f21ff17ea327a0f4c62e4d38acdbccf844348
Irtaza12 <irtazazulfiqar6tn@gmail.com>
2022-03-15 05:04:15 -0700

    Irtaza12

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
            userName= "Irtaza"
            userSecret="12345"
            renderChatFeed={ (chatAppProps) => <ChatFeed  {... chatAppProps} />}
-           
+
        />  
     )
     } 
